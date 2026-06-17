ef21047121550dda2739b4cfa0d2f784fe8e3bd6
Vijaya101 <vijayabadgujar0@gmail.com>
2026-06-17 16:17:54 +0530

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/clientsyncpro-client-dashboard-ui/",
-  plugins: [react()]
-})
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/clientsyncpro-client-dashboard-ui/"
+      : "/",
+  plugins: [react()],
+});
