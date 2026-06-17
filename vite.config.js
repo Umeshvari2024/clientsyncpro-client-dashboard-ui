04566e9844a62c2e7f8f585469ee111d9007f148
Vijaya101 <vijayabadgujar0@gmail.com>
2026-06-17 16:20:33 +0530

    Configure GitHub Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base:
-    process.env.NODE_ENV === "production"
+    mode === "github"
       ? "/clientsyncpro-client-dashboard-ui/"
       : "/",
   plugins: [react()],
-});
+}));
